4406205ca390f733ec5c9cd6db01ed6f7d051be8
Austin Muelrath <austinmuelrath@gmail.com>
2026-07-06 14:04:56 -0700

    refactor: move playwright config constants to constants.py

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -125,3 +125,19 @@ COOKIE_BANNER_REMOVE_SCRIPT = """
         .querySelectorAll('[class*="cookie"], [class*="consent"], [id*="cookie"], [id*="consent"]')
         .forEach(el => el.remove());
     """
+PW_HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "cross-site",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+}
+PW_USER_AGENT = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/120.0.0.0 Safari/537.36"
+)
+DEBUG_ENABLED = False
