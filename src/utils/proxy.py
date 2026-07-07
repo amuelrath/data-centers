372b641468b8530eb1a044a4386b5081d219e696
Austin Muelrath <austinmuelrath@gmail.com>
2026-07-06 20:10:36 -0700

    feat: update build_proxy to work with requests and playwright

--- src/utils/proxy.py
+++ src/utils/proxy.py
@@ -1,20 +1,33 @@
 import logging
 import os
-
-from dotenv import load_dotenv
+from typing import Literal
 
 logger = logging.getLogger(__name__)
 
-def build_proxy() -> str | None:
-    load_dotenv()
+
+def build_proxy(
+    returns: Literal["requests", "playwright"] = "requests",
+) -> dict[str, str] | None:
+    """
+
+    :param returns: The format to return the proxy in.
+    :return: If "requests" returns ``{ "http": ..., "https": ... }``.
+     If "playwright" returns ``{ "server": ..., "username":..., "password":... }``
+    """
     USERNAME = os.getenv("DECODO_USERNAME")
     PASSWORD = os.getenv("DECODO_PASSWORD")
 
     if not USERNAME or not PASSWORD:
         logger.info("No decodo username or password found in .env. Skipping proxy use.")
-        proxies = None
-    else:
-        proxy_url = f"https://{USERNAME}:{PASSWORD}@gate.decodo.com:10000"
-        proxies = {"http": proxy_url, "https": proxy_url}
+        return None
 
-    return proxies
+    server = "http://gate.decodo.com:10000"
+
+    if returns == "requests":
+        url = f"http://{USERNAME}:{PASSWORD}@gate.decodo.com:10000"
+        return {"http": url, "https": url}
+
+    if returns == "playwright":
+        return {"server": server, "username": USERNAME, "password": PASSWORD}
+
+    return None
